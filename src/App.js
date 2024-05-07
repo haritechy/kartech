e6200a6dbf5ffd24e59fd43b6572f3f63bdddab7
haritechy <168185029+haritechy@users.noreply.github.com>
2024-05-07 12:13:17 +0530

    commits

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Home } from './components/Home';
+
+
+
+
+
+function App() {
+    return (
+        <Router>
+            <div >
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                </Routes>
+            </div>
+        </Router>
+    );
+}
+
+export default App;
